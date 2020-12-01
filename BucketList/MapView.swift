15975c914d621e718b5c1b0a81668ad5c7f2c769
Rafael Calunga <rafaelcalunga@gmail.com>
2020-12-01 13:23:52 -0500

    Customizing MKMapView annotations

--- BucketList/MapView.swift
+++ BucketList/MapView.swift
@@ -10,6 +10,9 @@ import MapKit
 
 struct MapView: UIViewRepresentable {
     @Binding var centerCoordinate: CLLocationCoordinate2D
+    @Binding var selectedPlace: MKPointAnnotation?
+    @Binding var showingPlaceDetails: Bool
+    
     var annotations: [MKPointAnnotation]
     
     func makeUIView(context: Context) -> MKMapView {
@@ -39,6 +42,28 @@ struct MapView: UIViewRepresentable {
         func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
             parent.centerCoordinate = mapView.centerCoordinate
         }
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            let identifier = "Placemark"
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+            
+            if annotationView == nil {
+                annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                annotationView?.canShowCallout = true
+                annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            } else {
+                annotationView?.annotation = annotation
+            }
+            
+            return annotationView
+        }
+        
+        func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+            guard let placemark = view.annotation as? MKPointAnnotation else { return }
+            
+            parent.selectedPlace = placemark
+            parent.showingPlaceDetails = true
+        }
     }
 }
 
@@ -56,6 +81,8 @@ struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView(
             centerCoordinate: .constant(MKPointAnnotation.example.coordinate),
+            selectedPlace: .constant(MKPointAnnotation.example),
+            showingPlaceDetails: .constant(false),
             annotations: [MKPointAnnotation.example]
         )
     }
