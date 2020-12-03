d859cb04811088a5d55c00b0332093143c493a21
Rafael Calunga <rafaelcalunga@gmail.com>
2020-12-02 20:25:06 -0500

    Loading data and saving data using encryption

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -54,11 +54,12 @@ struct ContentView: View {
                 }
             )
         }
-        .sheet(isPresented: $showingEditScreen) {
+        .sheet(isPresented: $showingEditScreen, onDismiss: saveData) {
             if self.selectedPlace != nil {
                 EditView(placemark: self.selectedPlace!)
             }
         }
+        .onAppear(perform: loadData)
     }
     
     func createNewLocation() {
@@ -69,6 +70,32 @@ struct ContentView: View {
         
         self.selectedPlace = newLocation
         self.showingEditScreen = true
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func loadData() {
+        let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
+        
+        do {
+            let data = try Data(contentsOf: filename)
+            locations = try JSONDecoder().decode([CodableMKPointAnnotation].self, from: data)
+        } catch {
+            print("Unable to load saved data.")
+        }
+    }
+    
+    func saveData() {
+        do {
+            let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
+            let data = try JSONEncoder().encode(self.locations)
+            try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
+        } catch {
+            print("Unable to save data.")
+        }
     }
 }
 
